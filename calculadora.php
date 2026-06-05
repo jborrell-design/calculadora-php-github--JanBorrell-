073185977d4efb878e8fe839a7b3c5c04d2dac5e
Jan Borrell <jborrell@incamidemar.cat>
2026-06-05 16:48:44 +0200

    Afegida funció calcular() amb la lògica de càlcul

--- calculadora.php
+++ calculadora.php
@@ -1,20 +1,23 @@
 <?php
+function calcular($operacio, $primerNumero, $segonNumero) {
+    if ($operacio == "s") {
+        return $primerNumero + $segonNumero;
+    } else if ($operacio == "r") {
+        return $primerNumero - $segonNumero;
+    } else if ($operacio == "m") {
+        return $primerNumero * $segonNumero;
+    } else if ($operacio == "d") {
+        if ($segonNumero != 0) {
+            return $primerNumero / $segonNumero;
+        } else {
+            return "Error";
+        }
+    }
+}
 $operacio = $_POST['op'];
 $primerNumero = $_POST['n1'];
 $segonNumero = $_POST['n2'];
 
-if ($operacio == "s") {
-    $res = $primerNumero + $segonNumero;
-} else if ($operacio == "r") {
-    $res = $primerNumero - $segonNumero;
-} else if ($operacio == "m") {
-    $res = $primerNumero * $segonNumero;
-} else if ($operacio == "d") {
-    if ($segonNumero != 0) {
-        $res = $primerNumero / $segonNumero;
-    } else {
-        $res = "Error";
-    }
-}
+$res = calcular($operacio, $primerNumero, $segonNumero);
 echo $res;
 ?>
